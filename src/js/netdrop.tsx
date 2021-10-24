6428f11c7c183dc077b758f3d2bdaf2c6a149e99
afatduck <85923151+afatduck@users.noreply.github.com>
2021-10-24 08:26:23 +0200

    api switch

--- src/js/netdrop.tsx
+++ src/js/netdrop.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 import { Provider, useSelector } from 'react-redux'
 
@@ -8,11 +8,20 @@ import { HostForm } from './comps/host'
 import { LoginForm } from './comps/login'
 import { DirList } from './comps/dirlist'
 
+globalThis.apiLocation = localStorage.getItem('apiLocation') || 'http://calc.world:5008/api/'
+
 $(document).ready(() => {
 
   const App = () => {
 
     const { globals } = useSelector((state: RootState) => state)
+
+    const [localhost, setLocalhost]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(globalThis.apiLocation.includes('localhost'))
+    const switchApi = () => {
+      setLocalhost(!localhost)
+      localStorage.setItem('apiLocation', `http://${!localhost ? 'localhost' : 'calc.world'}:5008/api/`)
+      globalThis.apiLocation = localStorage.getItem('apiLocation')
+    }
 
     return (
       <div>
@@ -20,6 +29,8 @@ $(document).ready(() => {
         {(() => { if (globals.level != 'HOST') { return <LoginForm /> } })()}
         {(() => { if (globals.level == 'BROWSE') { return <DirList /> } })()}
         <h5 className='text-info mt-3'>{globals.error}</h5>
+        <button type="button" className="btn btn-info btn-sm" onClick={switchApi} >{`Switch to ${localhost ? 'online' : 'localhost'} api`}</button>
+        <br />
         <ResetButton />
       </div>
     )
